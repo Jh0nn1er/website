d55bde20c07d4767b47e84829fcc62bf6a44573f
jrangelfnz <93673278+jrangelfnz@users.noreply.github.com>
2022-02-18 12:14:39 -0500

    Add: Database context and connection string settings

---
+++ WebsiteForms/Database/DbSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace WebsiteForms.Database
+{
+    public static class DbSettings
+    {
+        public static string GetConnectionString()
+        {
+            string server = Environment.GetEnvironmentVariable("DB_SERVER") ?? "(localdb)\\MSSqlLocalDb";
+            string database = Environment.GetEnvironmentVariable("DB_NAME") ?? "WebsiteForms";
+            string user = Environment.GetEnvironmentVariable("DB_USER") ?? "";
+            string password = Environment.GetEnvironmentVariable("DB_PASSWORD") ?? "";
+            bool integratedSecurity = Boolean.Parse(Environment.GetEnvironmentVariable("DB_INTEGRATED_SECURITY") ?? "true");
+
+            return $"Server={server};Database={database};Integrated Security={integratedSecurity};user id={user};password={password}";
+        }
+    }
+}
---
+++ WebsiteForms/Database/WebsiteFormsContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+using WebsiteForms.Entities;
+
+namespace WebsiteForms.Database
+{
+    public class WebsiteFormsContext : DbContext
+    {
+        public WebsiteFormsContext() : base(DbSettings.GetConnectionString())
+        {
+        }
+
+        public virtual DbSet<RequestType> RequestTypes { get; set; }
+
+        public virtual DbSet<Request> Requests { get; set; }
+
+        public virtual DbSet<User> Users { get; set; }
+    }
+}
