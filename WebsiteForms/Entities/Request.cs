8114824fad22dd88ff7f38a552309dff5ac32eb9
jrangelfnz <93673278+jrangelfnz@users.noreply.github.com>
2022-02-18 12:13:34 -0500

    Add: BaseEntity, Request, RequestType and User entities

---
+++ WebsiteForms/Entities/Request.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebsiteForms.Entities
+{
+    public class Request : BaseEntity
+    {
+        [Key]
+        public int RequestId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string FullName { get; set; }
+
+        [Required]
+        [MaxLength(13)]
+        public string Identifier { get; set; }
+
+        [Required]
+        [MaxLength(15)]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [MaxLength(10)]
+        public string Credit { get; set; }
+
+        [MaxLength(50)]
+        public string ModificationType { get; set; }
+
+        public int? Term { get; set; }
+
+        public DateTime NewPaymentDate { get; set; }
+
+        [MaxLength(50)]
+        public string LossOccuranceType { get; set; }
+
+        [MaxLength(50)]
+        public string VehicleChanges { get; set; }
+
+        [MaxLength(6)]
+        public string LicensePlate { get; set; }
+
+        public DateTime PaymentDate { get; set; }
+
+        [MaxLength(50)]
+        public string ProcedureType { get; set; }
+
+        [MaxLength(2048)]
+        public string PolicyPDFURL { get; set; }
+
+        [MaxLength(50)]
+        public string PQRType { get; set; }
+
+        [MaxLength(50)]
+        public string PQRComment { get; set; }
+
+        [Required]
+        public int RequestTypeId { get; set; }
+        public virtual RequestType RequestType { get; set; }
+    }
+}
