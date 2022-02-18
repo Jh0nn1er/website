3971bb3827bfc2faa601a1199e98565647521b06
jrangelfnz <93673278+jrangelfnz@users.noreply.github.com>
2022-02-18 12:15:04 -0500

    Add: Hashing helper

---
+++ WebsiteForms/Helpers/Hashing.cs
@@ -0,0 +1,11 @@
+﻿using BCript = BCrypt.Net.BCrypt;
+
+namespace WebsiteForms.Helpers
+{
+    public static class Hashing
+    {
+        public static string GetRandomSalt(int saltRounds = 12) => BCript.GenerateSalt(saltRounds);
+        public static string HashString(string str) => BCript.HashPassword(str);
+        public static bool Verify(string str, string hash) => BCript.Verify(str, hash);
+    }
+}
