85b5e5278bbabccecb94f7790821be2920b22c7a
MaicolPena <89145026+MaicolPena@users.noreply.github.com>
2022-02-21 15:59:00 -0500

    Change(request): Finish save request

--- WebsiteForms/API/v1/Controllers/RequestsController.cs
+++ WebsiteForms/API/v1/Controllers/RequestsController.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebsiteForms.API.v1.Models.Requests;
 using WebsiteForms.Authorization;
+using WebsiteForms.Database.Entities;
+using WebsiteForms.Repositories.RequestRepository;
 using WebsiteForms.Repositories.RequestTypesRepository;
+using WebsiteForms.Services.PolicyService;
+using WebsiteForms.Services.RequestService;
 
 namespace WebsiteForms.API.v1.Controllers
 {
@@ -12,10 +16,12 @@ namespace WebsiteForms.API.v1.Controllers
     public class RequestsController : ControllerBase
     {
         private readonly IRequestTypeRepository _requestTypeRepository;
+        private readonly IRequestService _requestService;
 
-        public RequestsController(IRequestTypeRepository requestTypeRepository)
+        public RequestsController(IRequestTypeRepository requestTypeRepository, IRequestService requestService)
         {
             _requestTypeRepository = requestTypeRepository;
+            _requestService = requestService;
         }
 
         [HttpGet]
@@ -27,9 +33,30 @@ namespace WebsiteForms.API.v1.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create([FromForm] RequestRequest req)
+        public async Task<IActionResult> Create([FromForm] RequestRequest req)
         {
-            return Ok(req);
+            var newRequest = new Request()
+            {
+                FullName = req.FullName,
+                Identifier = req.Identifier,
+                PhoneNumber = req.PhoneNumber,
+                Email = req.Email,
+                Credit = req.Credit,
+                ModificationType = req.ModificationType,
+                Term = req.Term,
+                NewPaymentDate = req.NewPaymentDate,
+                LossOccuranceType = req.LossOccuranceType,
+                VehicleChanges = req.VehicleChanges,
+                LicensePlate = req.LicensePlate,
+                PaymentDate = req.PaymentDate,
+                ProcedureType = req.ProcedureType,
+                PQRType = req.PQRType,
+                PQRComment = req.PQRComment,
+                RequestTypeId = req.RequestTypeId,
+            };
+            await _requestService.Create(newRequest, req.Policy);
+
+            return NoContent();
         }
     }
 }
