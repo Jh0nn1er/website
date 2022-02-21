32a0a5e407797e0e87d0bd0a09ba5c76b48a15b8
MaicolPena <89145026+MaicolPena@users.noreply.github.com>
2022-02-21 11:31:54 -0500

    Add: User service

---
+++ WebsiteForms/Services/UserService/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using WebsiteForms.Database.Entities;
+
+namespace WebsiteForms.Services.UserService
+{
+    public interface IUserService
+    {
+        User? Authenticate(string username, string password);
+    }
+}
---
+++ WebsiteForms/Services/UserService/UserService.cs
@@ -0,0 +1,26 @@
+﻿using WebsiteForms.Database.Entities;
+using WebsiteForms.Repositories.UserRepository;
+using WebsiteForms.Helpers;
+
+namespace WebsiteForms.Services.UserService
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public User? Authenticate(string username, string password)
+        {
+            var user = _userRepository.GetByUsername(username);
+            if (user == null) return null;
+
+            bool isValid = Hashing.Verify(password, user.Password);
+            if (!isValid) return null;
+
+            return user;
+        }
+    }
+}
