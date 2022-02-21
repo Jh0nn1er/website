cd06ba036a28d857d0c446b5c7bde22611969d86
MaicolPena <89145026+MaicolPena@users.noreply.github.com>
2022-02-21 11:41:17 -0500

    Add: Request types controller with method to get all records

---
+++ WebsiteForms/API/v1/Controllers/RequestsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebsiteForms.Authorization;
+using WebsiteForms.Repositories.RequestTypesRepository;
+
+namespace WebsiteForms.API.v1.Controllers
+{
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiVersion("1.0")]
+    [ApiController]
+    [JwtAuthorize]
+    public class RequestsController : ControllerBase
+    {
+        private readonly IRequestTypeRepository _requestTypeRepository;
+
+        public RequestsController(IRequestTypeRepository requestTypeRepository)
+        {
+            _requestTypeRepository = requestTypeRepository;
+        }
+
+        [HttpGet]
+        [Route("types")]
+        public IActionResult GetTypes()
+        {
+            var requestTypes = _requestTypeRepository.GetAll();
+            return Ok(requestTypes);
+        }
+
+        [HttpPost]
+        public IActionResult Create(IFormCollection data, IFormFile? pdfFile = null)
+        {
+            return Ok(data);
+        }
+    }
+}
