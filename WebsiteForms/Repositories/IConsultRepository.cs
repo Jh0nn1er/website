79988293545360b680b626f1abc07caa769e4ce2
MaicolPena <89145026+MaicolPena@users.noreply.github.com>
2022-02-21 11:29:12 -0500

    Add: Base consult interface to repositories

---
+++ WebsiteForms/Repositories/IConsultRepository.cs
@@ -0,0 +1,10 @@
+﻿using WebsiteForms.Database;
+
+namespace WebsiteForms.Repositories
+{
+    public interface IConsultRepository<T>
+    {
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+    }
+}
