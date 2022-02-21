f47d3020bbb9ca1ac545b52515179f7fa91a4d0b
MaicolPena <89145026+MaicolPena@users.noreply.github.com>
2022-02-21 11:31:34 -0500

    Add: User controller with authentication method

---
+++ WebsiteForms/API/v1/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebsiteForms.API.v1.Authenticate.Models;
+using WebsiteForms.Services.UserService;
+using WebsiteForms.Authorization;
+using WebsiteForms.Helpers;
+using WebsiteForms.Repositories.UserRepository;
+
+namespace WebsiteForms.API.v1.Controllers
+{
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiVersion("1.0")]
+    [ApiController]
+    [JwtAuthorize]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        [Route("authenticate")]
+        [AllowAnonymous]
+        public IActionResult Authenticate(AuthenticateRequest req)
+        {
+            var user = _userService.Authenticate(req.Username, req.Password);
+            if (user == null)
+                return BadRequest(new
+                {
+                    msg = "Username or password incorrect"
+                });
+
+            return Ok(new AuthenticateResponse() { Token = JwtUtils.Generate(user.UserID) });
+        }
+    }
+}
