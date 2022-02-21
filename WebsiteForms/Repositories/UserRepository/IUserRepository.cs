96e7ac96051f73bafc3a8d6e7eee497a29fabfa9
MaicolPena <89145026+MaicolPena@users.noreply.github.com>
2022-02-21 11:29:44 -0500

    Add: interface and class to user repository

---
+++ WebsiteForms/Repositories/UserRepository/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using WebsiteForms.Database.Entities;
+
+namespace WebsiteForms.Repositories.UserRepository
+{
+    public interface IUserRepository: IConsultRepository<User>
+    {
+        User? GetByUsername(string username);
+    }
+}
---
+++ WebsiteForms/Repositories/UserRepository/UserRepository.cs
@@ -0,0 +1,41 @@
+﻿using WebsiteForms.Database;
+using WebsiteForms.Database.Entities;
+
+namespace WebsiteForms.Repositories.UserRepository
+{
+    public class UserRepository : IUserRepository
+    {
+        public IEnumerable<User> GetAll()
+        {
+            List<User> users;
+            using (var db = new WebsiteFormsContext())
+            {
+                users = db.Users.ToList();
+            }
+
+            return users;
+        }
+
+        public User GetById(int id)
+        {
+            User user;
+            using (var db = new WebsiteFormsContext())
+            {
+                user = db.Users.Find(id);
+            }
+
+            return user;
+        }
+
+        public User? GetByUsername(string username)
+        {
+            User? user;
+            using (var db = new WebsiteFormsContext())
+            {
+                user = db.Users.Where(u => u.Username == username).FirstOrDefault();
+            }
+
+            return user;
+        }
+    }
+}
