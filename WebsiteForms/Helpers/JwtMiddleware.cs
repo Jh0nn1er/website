5101a97c54e4d478b07e18515424f20f6fc67b4c
MaicolPena <89145026+MaicolPena@users.noreply.github.com>
2022-02-21 11:27:08 -0500

    Add: Middleware to create user item in http conext with token

---
+++ WebsiteForms/Helpers/JwtMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using WebsiteForms.Repositories.UserRepository;
+
+namespace WebsiteForms.Helpers
+{
+    public class JwtMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public JwtMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context, IUserRepository userRepository)
+        {
+            var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+            var userId = JwtUtils.Verify(token);
+
+            if (userId != null)
+            {
+                context.Items["User"] = userRepository.GetById(userId.Value);
+            }
+
+            await _next(context);
+        }
+    }
+}
