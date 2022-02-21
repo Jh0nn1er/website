c85eb812b40bca2648af49a78a82f38de00f1957
MaicolPena <89145026+MaicolPena@users.noreply.github.com>
2022-02-21 11:34:58 -0500

    Add: App settings

---
+++ WebsiteForms/AppSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace WebsiteForms
+{
+    public class AppSettings
+    {
+        public string SecretKey { get => Environment.GetEnvironmentVariable("SECRET_KEY"); }
+        public string AudienceToken { get => Environment.GetEnvironmentVariable("AUDIENCE_TOKEN"); }
+        public string IssuerToken { get => Environment.GetEnvironmentVariable("ISSUER_TOKEN"); }
+        public string ExpireTokenMinutes { get => Environment.GetEnvironmentVariable("EXPIRE_TOKEN_MINUTES"); }
+    }
+}
