52814bef578e0e517400cb474937bb4082516d16
MaicolPena <89145026+MaicolPena@users.noreply.github.com>
2022-02-22 11:44:24 -0500

    Refactor: Dependency injection according to until of work

--- WebsiteForms/Program.cs
+++ WebsiteForms/Program.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Mvc.Versioning;
 using dotenv.net;
 using WebsiteForms.Helpers;
 using WebsiteForms.Services.UserService;
-using WebsiteForms.Repositories.UserRepository;
-using WebsiteForms.Repositories.RequestTypesRepository;
 using WebsiteForms.Services.PolicyService;
-using WebsiteForms.Repositories.RequestRepository;
 using WebsiteForms.Services.RequestService;
+using WebsiteForms.Services.RequestTypeService;
+using WebsiteForms.Repositories.Contracts;
+using WebsiteForms.Repositories;
+using WebsiteForms.Database;
+using System.Data.Entity;
 
 DotEnv.Load();
 
@@ -34,11 +36,12 @@ builder.Services.AddVersionedApiExplorer(options =>
 });
 
 
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<IRequestTypeRepository, RequestTypeRepository>();
-builder.Services.AddScoped<IRequestRepository, RequestRepository>();
+builder.Services.AddSingleton<IUnitOfWork>(x => new UnitOfWork(new WebsiteFormsContext()));
+
+builder.Services.AddScoped<DbContext, WebsiteFormsContext>();
 builder.Services.AddScoped<IRequestService, RequestService>();
 builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IRequestTypeService, RequestTypeService>();
 builder.Services.AddScoped<IPolicyService, FileService>();
 
 builder.Services.AddCors();
