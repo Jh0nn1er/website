25fdbc40a6214eca0416f0e59437ac9945114d17
jrangelfnz <93673278+jrangelfnz@users.noreply.github.com>
2022-02-25 14:44:32 -0500

    Refactor(migration): Improve seed method, fix error inserting duplicate data

--- WebsiteForms/Migrations/Configuration.cs
+++ WebsiteForms/Migrations/Configuration.cs
@@ -1,21 +1,67 @@
 ﻿namespace WebsiteForms.Migrations
 {
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
-    using System.Linq;
+    using System.Diagnostics;
+    using System.Reflection;
+    using WebsiteForms.Database;
+    using WebsiteForms.Database.Entities;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<WebsiteForms.Database.WebsiteFormsContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<WebsiteFormsContext>
     {
+        private readonly string[] EXCEPT_COLUMNS_UPDATE = new string[] { "CreatedAt" };
+        private  WebsiteFormsContext Context { get; set; }
+
+
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(WebsiteForms.Database.WebsiteFormsContext context)
+        protected override void Seed(WebsiteFormsContext context)
         {
-            context.RequestTypes.AddRange(Initializer.GetRequestTypes());
-            context.Users.AddRange(Initializer.GetUsers());
+            Context = context;
+
+            AddOrUpdateRange(Initializer.GetUsers());
+            AddOrUpdateRange(Initializer.GetRequestTypes());
+        }
+
+        private void AddOrUpdateRange<T>(IEnumerable<T> entities) where T: BaseEntity
+        {
+            var entitiesList = (List<T>)entities;
+            entitiesList.ForEach(x => AddOrUpdate(x));
+        }
+
+        private void AddOrUpdate<T>(T entity) where T : BaseEntity
+        {
+            var DbSet = Context.Set<T>();
+            var originalEntity = DbSet.FirstOrDefault(x => x.Id == entity.Id);
+
+            if (originalEntity == null) Add(entity);
+            else Modify(originalEntity, entity);
+        }
+
+        private void Modify<T>(T originalEntity, T newEntity) where T: BaseEntity
+        {
+            PropertyInfo[] originalProperties = originalEntity.GetType().GetProperties();
+            PropertyInfo[] newProperties = newEntity.GetType().GetProperties();
+
+            foreach (PropertyInfo property in originalProperties)
+            {
+                var found = newProperties.FirstOrDefault(p => !EXCEPT_COLUMNS_UPDATE.Any(x => x == p.Name) && p.Name == property.Name);
+                if(found != null)
+                {
+                    property.SetValue(originalEntity, found.GetValue(newEntity));
+                }
+            }
+        }
+
+        private void Add<T>(T entity) where T: BaseEntity
+        {
+            var DbSet = Context.Set<T>();
+            DbSet.Add(entity);
         }
     }
 }
--- WebsiteForms/Migrations/Initializer.cs
+++ WebsiteForms/Migrations/Initializer.cs
@@ -9,54 +9,67 @@ namespace WebsiteForms.Migrations
         {
             new RequestType()
             {
+                Id = 1,
                 Name = "Paz y salvo",
             },
             new RequestType()
             {
+                Id = 2,
                 Name = "Levantamientos de prenda",
             },
             new RequestType()
             {
+                Id = 3,
                 Name = "Certificaciones",
             },
             new RequestType()
             {
+                Id = 4,
                 Name = "Reestructuraciones",
             },
             new RequestType()
             {
+                Id = 5,
                 Name = "Cambio de fecha",
             },
             new RequestType()
             {
+                Id = 6,
                 Name = "Siniestros",
             },
             new RequestType()
             {
+                Id = 7,
                 Name = "Cambio de vehiculo",
             },
             new RequestType()
             {
+                Id = 8,
                 Name = "Cambios al vehiculo",
             },
             new RequestType()
             {
+                Id = 9,
                 Name = "Solicitud documentos de desembolsos",
             },
             new RequestType()
             {
+                Id = 10,
                 Name = "Liquidacion total",
             },
             new RequestType()
             {
+                Id = 11,
                 Name = "Subragaciones",
             },
             new RequestType()
             {
+                Id = 12,
                 Name = "Tramites de seguros",
             },
             new RequestType()
             {
+                Id = 13,
                 Name = "Peticiones, quejas y reclamos",
             },
         };
@@ -64,8 +77,9 @@ namespace WebsiteForms.Migrations
         {
             new User()
             {
+                Id = 1,
                 Username = "Finanzauto",
-                Password = Hashing.HashString("Admin"),
+                Password = Hashing.HashString("Admin")
             }
         };
 
