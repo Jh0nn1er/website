4b0bfd136bde11f26ba4415c5cbb6505c1e48a70
jrangelfnz <93673278+jrangelfnz@users.noreply.github.com>
2022-03-01 14:42:44 -0500

    Change: Remove response from request creation

--- WebsiteForms/API/v1/Controllers/RequestsController.cs
+++ WebsiteForms/API/v1/Controllers/RequestsController.cs
@@ -81,7 +81,7 @@ namespace WebsiteForms.API.v1.Controllers
             else insertedId = _requestService.Add(newRequest);
 
             string uri = $"{Request.Scheme}://{Request.Host.Value}{Request.Path.Value}/{insertedId}";
-            if (insertedId != null) return Created(uri, new { message = "Successfully Created" });
+            if (insertedId != null) return Created(uri, null);
 
             return StatusCode(500);
         }
