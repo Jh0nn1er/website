2bcf87bd1b2b180d46dd53764337ea9d3390f242
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-10-24 15:07:50 -0500

    Add(request): debug conditional to autorization

--- WebsiteForms/API/v1/Controllers/RequestsController.cs
+++ WebsiteForms/API/v1/Controllers/RequestsController.cs
@@ -12,7 +12,9 @@ namespace WebsiteForms.API.v1.Controllers
     [Route("api/v{version:apiVersion}/[controller]")]
     [ApiVersion("1.0")]
     [ApiController]
-    [Authorization]
+#if !DEBUG
+[Authorization]
+#endif
     public class RequestsController : ControllerBase
     {
         private readonly IRequestService _requestService;
