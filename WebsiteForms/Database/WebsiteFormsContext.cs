f8e677403322edc38f822a8b9525787f980f52c4
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-11-08 14:53:45 -0600

    Add habes data table

--- WebsiteForms/Database/WebsiteFormsContext.cs
+++ WebsiteForms/Database/WebsiteFormsContext.cs
@@ -16,5 +16,6 @@ namespace WebsiteForms.Database
 
         public virtual DbSet<User> Users { get; set; }
         public DbSet<Log> Log { get; set; }
+        public DbSet<HabeasData> HabeasData { get; set; }
     }
 }
---
+++ WebsiteForms/Migrations/202211081725266_HabeasData.cs
@@ -0,0 +1,16 @@
+﻿namespace WebsiteForms.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class HabeasData : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
