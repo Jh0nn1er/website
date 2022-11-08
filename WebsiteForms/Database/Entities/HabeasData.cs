a6e14b7af7222cedbb69ffdc75127962b2bcb9f1
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-11-08 14:53:23 -0600

    Add habeas data model

---
+++ WebsiteForms/Database/Entities/HabeasData.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebsiteForms.Database.Entities
+{
+    public class HabeasData
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(15)]
+        public string SQRType{ get; set; }
+        public bool? DeleteOfComercialBases { get; set; }
+        public bool? DeleteOfCampaignBases { get; set; }
+        public bool? DeleteOfEventBases { get; set; }
+        [Required, MaxLength(10)]
+        public string LandLine{ get; set; }
+        [Required, MaxLength(8000)]
+        public string Reason{ get; set; }
+        [EmailAddress]
+        public string? EmailNotification{ get; set; }
+        [MaxLength(200)]
+        public string? AddressNotification { get; set; }
+        [MaxLength(15)]
+        public string? CellPhoneNotification{ get; set; }
+        public virtual Request Request { get; set; }
+    }
+}
