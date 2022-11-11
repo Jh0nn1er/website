edbb284a6fbb109363afc59bca68f6404ce1e51c
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-11-11 11:28:22 -0600

    Add configuration model

---
+++ WebsiteForms/Database/Entities/Configuration.cs
@@ -0,0 +1,9 @@
+﻿namespace WebsiteForms.Database.Entities
+{
+    public class Configuration : BaseEntity
+    {
+        public string Key { get; set; }
+        public string Description { get; set; }
+        public string Value { get; set; }
+    }
+}
