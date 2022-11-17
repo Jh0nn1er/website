d4c9902afcc2fb707fbd47cabc6883d0892baa32
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-11-17 15:41:52 -0600

    Change: SQR Type ubicaction

---
+++ WebsiteForms/Migrations/202211152138451_FixSQR.cs
@@ -0,0 +1,22 @@
+﻿namespace WebsiteForms.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class FixSQR : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Requests", "SQRType", c => c.String(maxLength: 15));
+            AlterColumn("dbo.HabeasDatas", "LandLine", c => c.String(maxLength: 10));
+            DropColumn("dbo.HabeasDatas", "SQRType");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.HabeasDatas", "SQRType", c => c.String(nullable: false, maxLength: 15));
+            AlterColumn("dbo.HabeasDatas", "LandLine", c => c.String(nullable: false, maxLength: 10));
+            DropColumn("dbo.Requests", "SQRType");
+        }
+    }
+}
