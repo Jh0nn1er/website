4038ee00da829d06502541bcf8d334117d6045b9
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-11-17 15:47:22 -0600

    Add configuration repository

---
+++ WebsiteForms/Repositories/Entity/ConfigurationRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+using WebsiteForms.Database.Entities;
+using WebsiteForms.Repositories.Contracts;
+
+namespace WebsiteForms.Repositories.Entity
+{
+    public class ConfigurationRepository : EntityRepository<Configuration>, IConfigurationRepository
+    {
+        public ConfigurationRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
