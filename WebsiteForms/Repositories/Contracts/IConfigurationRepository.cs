170c7e3f37f80c515cdca40876af908f3e024afa
Maicol Peña <maicol.pena@finanzauto.com.co>
2022-11-17 15:47:44 -0600

    Add configuration repository interface

---
+++ WebsiteForms/Repositories/Contracts/IConfigurationRepository.cs
@@ -0,0 +1,8 @@
+﻿using WebsiteForms.Database.Entities;
+
+namespace WebsiteForms.Repositories.Contracts
+{
+    public interface IConfigurationRepository : ISimpleConsultRepository<Configuration>, ILambdaConsultRepository<Configuration>, ICreateRepository<Configuration>
+    {
+    }
+}
